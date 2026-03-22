b239dc642d4134c552ec6ad24816ee02d6a1139d
raoshreeharsha-devops <raoshreeharsha@gmail.com>
2026-03-22 16:56:31 +0530

    Check connection

--- Learn-TF/main.tf
+++ Learn-TF/main.tf
@@ -7,10 +7,15 @@ terraform {
   }
 }
 resource "aws_instance" "main" {
-  ami = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.small"
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "websample"
   }
 }
+  provider = "aws" {
+        region = "us-east-1"
+}
+
+provider
