2514b77a0cd397905eb905146dcc90f5ba6e93be
raoshreeharsha-devops <raoshreeharsha@gmail.com>
2026-03-22 18:57:33 +0530

    Check connection

--- Learn-TF/main.tf
+++ Learn-TF/main.tf
@@ -1,15 +1,23 @@
-resource "aws_instance" "test" {
-  count = var.instance_cnt
+resource "aws_instance" "test_loop" {
+  count = 5
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-00e30f54573caca07"]
 
   tags = {
-    Name = var.instance_name
+    Name = var.components[count.index]
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_route53_record" "test_dns" {
+  name    = var.components[count.index]
+  type    = "A"
+  zone_id = "Z0201776NY87AL3MW923"
+  ttl = 30
+  records = [aws_instance.test_loop.private_ip]
 }
 
--- Learn-TF/variable.tf
+++ Learn-TF/variable.tf
@@ -2,12 +2,3 @@ variable "components" {
    default = ["frontend","postgresql","portfolio-services","auth-service","analytics"]
 }
 
-variable "instance_cnt" {
-  description = "No. of EC2 instance"
-  type        = number
-}
-
-variable "instance_name" {
-  description = "Instance name"
-  type = string
-}
