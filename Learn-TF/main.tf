135f5aaee9e1e07da8c89aff5b5f8e8a29d44869
raoshreeharsha-devops <raoshreeharsha@gmail.com>
2026-03-22 16:31:12 +0530

    Check connection

--- Learn-TF/main.tf
+++ Learn-TF/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
 resource "aws_instance" "web" {
   ami = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -6,3 +14,5 @@ resource "aws_instance" "web" {
     Name = "websample"
   }
 }
+
+resource "null_resource" "test" {}
