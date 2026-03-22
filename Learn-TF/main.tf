e2b570bec7d5e0701ad7902c54bb444d0d011415
raoshreeharsha-devops <raoshreeharsha@gmail.com>
2026-03-22 17:21:04 +0530

    Check connection

--- Learn-TF/main.tf
+++ Learn-TF/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
 resource "aws_instance" "main" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
@@ -5,6 +13,11 @@ resource "aws_instance" "main" {
   tags = {
     Name = "websample"
   }
+}
+
+resource "aws_connect_security_profile" "SGTest" {
+  instance_id = "sg-00e30f54573caca07"
+  name        = "AllAllow"
 }
 
 provider "aws" {
