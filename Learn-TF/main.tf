282163e40b2e9a51cc533ccefd3c4cdfe854b204
raoshreeharsha-devops <raoshreeharsha@gmail.com>
2026-03-22 17:27:10 +0530

    Check connection

--- Learn-TF/main.tf
+++ Learn-TF/main.tf
@@ -1,15 +1,11 @@
 resource "aws_instance" "main" {
   ami           = "ami-0220d79f3f480ecf5"
   instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-00e30f54573caca07"]
 
   tags = {
     Name = "websample"
   }
-}
-
-resource "aws_connect_security_profile" "SGTest" {
-  instance_id = "sg-00e30f54573caca07"
-  name        = "AllAllow"
 }
 
 provider "aws" {
